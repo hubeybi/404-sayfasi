ff061018e299a280c4a37b93d36f5ecbd7ad04e1
Barış Ayar <barisayarr@gmail.com>
2017-06-22 16:08:15 +0300

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8"/>
+  <title> Barış Ayar | Sayfa bulunamadı </title>
+  <!-- Load CSS for the page -->
+  <link rel="stylesheet" href="/404.css"/>
+</head>
+
+<body data-hue="0">
+  <!-- Background -->
+  <div id="scene"></div>
+  <!-- Foreground  -->
+  <div class="main">
+    <h1>404</h1>
+    <p>Böyle bir şey yok :(</p>
+    <a href="https://www.hubeybi.com/">Ana Sayfaya Dön</a>
+  </div>
+
+  <!-- Magic -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/1.1.1/chroma.min.js"></script>
+  <script type="text/javascript" src="/404.js"></script>
+</body>
+
+</html>
